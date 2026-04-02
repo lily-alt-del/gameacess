8f0978d0f357da381f119497f77068223fe8ab92
lily-alt-del <santos.miguel1@aluno.ifsp.edu.br>
2026-04-02 17:27:49 -0300

    header1

--- src/Components/NavBar/MiddleNav.tsx
+++ src/Components/NavBar/MiddleNav.tsx
@@ -1,39 +1,47 @@
-'use client';
-import Link from 'next/link';
-import Image from 'next/image';
+'use client'
+
 import { useState } from 'react';
+import Image from 'next/image';
+import { CartItem } from '@/types';
+
+interface HeaderProps {
+  cartItems: CartItem[]
+  onRemoveFromCart: (id: number) => void
+  onUpdateQuantity: (id: number, quantity: number) => void
+  searchQuery: string
+  setSearchQuery: (query: string) => void
+}
 
 export default function MiddleNav() {
-  const [query, setQuery] = useState('');
-
+  
   return (
-    <div className='w-full bg-[var(--prim)]'>
-      <div className='flex items-center justify-between px-[8%] py-3 lg:px-[16%]'>
-
-        {/* Logo  */}
-        <Link href='/' className='inline-block'>
-          <div className='group relative h-12 w-40'>
-
-            {/* Logo2 default */}
-            <Image
-              src='/logoaccess2.png'
-              alt='Logo Access 2'
-              width={100}
-              height={100}
-              className='block opacity-100 transition-opacity duration-200 group-hover:opacity-0'
-            />
-
-            {/* Logo1 Hover */}
-            <Image
-              src='/logoaccess1.gif'
-              alt='Logo Access 1'
-              width={100}
-              height={100}
-              className='absolute inset-0 opacity-0 transition-opacity duration-200 group-hover:opacity-100'
-            />
-          </div>
-        </Link>
+    <div className='items-center justify-between border-b border-purple-800' style={{ padding: '20px', paddingInline: '15%', display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+      {/* LOGO */}
+      <div className='flex items-center gap-5'>
+        {/* Logo image */}
+        <Image
+          src='/logoaccess2.png'
+          alt='Access Game Logo'
+          width={100}
+          height={100}
+        />
+        {/* Logotipo */}
+        <span className='gradient-text font-anton hidden text-4xl font-anton text-purple-200 font-bold sm:inline'>
+          AccessGame
+        </span>
       </div>
+      {/* CARRINHO E PERFIL */}
+      <div className='flex items-center gap-5' style={{ paddingRight: '30px' }}>
+          {/* Ícone de carrinho */}
+          <button className=' text-white rounded-xl w-10 h-10 items-center hover:bg-(--prim-light) '>
+            <i className="bi bi-cart2 text-2xl"></i>
+          </button>
+          {/* Ícone de perfil */}
+          <button className=' text-white rounded-xl w-10 h-10 items-center hover:bg-(--prim-light) '>
+            <i className="bi bi-person text-2xl"></i>
+          </button>
+      </div>
+      
     </div>
   );
 }
